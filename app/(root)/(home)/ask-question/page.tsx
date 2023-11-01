4c80f8c47ab5eb25ff78385f406b7c6a3a7ae0ab
krish-98 <muralikrishnanm98@gmail.com>
2023-11-01 18:45:46 +0530

    route group added

---
+++ app/(root)/(home)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Askquestion = () => {
+  return <div>Askquestion</div>
+}
+
+export default Askquestion
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css"
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS 13 I&apos;m coming</h1>
-    </div>
-  )
-}
